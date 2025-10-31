83e5723ae10782888ddc5f90f0fe1f9ad34b36e5
desty <dimonmikryukov@gmail.com>
2025-11-01 00:18:05 +0400

    feat(menu): add logo Rostic's & profile button

--- app/src/main/java/com/example/rostics/fragments/MenuFragment.java
+++ app/src/main/java/com/example/rostics/fragments/MenuFragment.java
@@ -1,18 +1,27 @@
 package com.example.rostics.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.example.rostics.R;
 
 public class MenuFragment extends Fragment {
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+
+        ImageButton btnProfile = view.findViewById(R.id.btn_profile);
+        btnProfile.setOnClickListener(v -> {
+        });
+
+        return view;
     }
 }
